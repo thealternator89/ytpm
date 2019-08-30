ae4d21cec4616b9100726355fad95cb2deee7101
Mark Benson <thealternator89@gmail.com>
2019-08-31 08:59:40 +1200

    Enable cors support for API endpoints

--- src/server/endpoints/Api.ts
+++ src/server/endpoints/Api.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import * as cors from 'cors';
 
 import { clientApiRouter } from './api/Client';
 import { internalApiRouter } from './api/Internal';
 import { playerApiRouter } from './api/Player';
 
 const router = Router();
+
+// enable cors for api endpoints
+router.use(cors());
 
 router.use('/client', clientApiRouter);
 router.use('/internal', internalApiRouter);
