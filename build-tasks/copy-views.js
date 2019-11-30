c7eb173d2e5e05c808b8bd4b58beffc49b50248c
Mark Benson <thealternator89@gmail.com>
2019-11-30 14:35:01 +1300

    Add discovery (playlists and channels)

--- build-tasks/copy-views.js
+++ build-tasks/copy-views.js
@@ -1,3 +1,4 @@
 var copyfiles = require('copyfiles');
 
 copyfiles(['views/**/*', 'dist'], 0, () => undefined);
+copyfiles(['src/server/data/discovery/*', 'dist/data/discovery'], -1, () => undefined);
